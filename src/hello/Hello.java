d260ab6b65fc947566743ac4e7bc9b4bf1628c46
Eugene <ekononchuk22@ymail.com>
2022-01-24 11:25:17 -0500

    hello world

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("some print added");
         System.out.println("changes made");
+        System.out.println("hello world");
     }
 }
